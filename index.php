ffe6824f91b2ea3135180db4f9fe0818dfcc5af9
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2023-02-17 22:05:07 +0100

    push using script

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page D'accueil</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="bg-secondary p-1 m-2">
+        <ul class="nav justify-content-end ">
+            <li class="nav-item">
+            <a class="nav-link text-white" href="./Signin.php" > <img src="./assets/login.png" style="width: 20px;" alt="logo"> Login</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link text-white" href="./signup.php"><img src="./assets/signup.png" style="width: 20px;" alt="logo"> Sign up</a>
+            </li>
+        </ul>
+    </div>
+
+
+        <h1 class="mt-5 text-center">Bienvenu Dans Votre École sur Internet </h1>
+
+        <img src="./assets/athena.jpg" style="width:900px ;height:600px;"  class="img-fluid mx-auto d-block" alt="">
+
+</body>
+</html>
